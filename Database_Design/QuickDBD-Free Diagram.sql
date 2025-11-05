92addb99c0f6670cb57b546556fd8bb93db0730c
dani-aranha <dani_0102@yahoo.com.br>
2025-11-05 12:21:02 -0400

    Database class
    Teach on Nov 5th

---
+++ Database_Design/QuickDBD-Free Diagram.sql
@@ -0,0 +1,26 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/FX7z5t
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+SET XACT_ABORT ON
+
+BEGIN TRANSACTION QUICKDBD
+
+DROP Table dbo.ProductDim
+GO
+
+CREATE TABLE [ProductDim] (
+    [ProductID] int  NOT NULL ,
+    [productName] varchar(50)  NOT NULL ,
+    [ProductCategoryID] int  NOT NULL ,
+    [ProductCategory] varchar(50)  NOT NULL ,
+    [ProductCategoryDescription] varchar(MAX)  NOT NULL ,
+    [SupplierID] int  NOT NULL ,
+    [Supplier] varchar(50)  NOT NULL ,
+    CONSTRAINT [PK_ProductDim] PRIMARY KEY CLUSTERED (
+        [ProductID] ASC
+    )
+)
+
+COMMIT TRANSACTION QUICKDBD
